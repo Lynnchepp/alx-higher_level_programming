dfeba80770059950d2e03ef7fbc01b4adfac1d30
lynnchepp <lynn286035@gmail.com>
2023-12-07 01:40:00 -0800

    2-uniq_add.py

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def uniq_add(my_list[]):
-    unique_list = set(my_list) # convert the list to a set to grt unique elements
-    return sum(unique_list)
+    unique_set = set(my_list) # convert the list to a set to grt unique elements
+    return sum(unique_ set)
