19866b8d33abf446f709e77bd0b6ce35fb157857
lynnchepp <lynn286035@gmail.com>
2023-12-08 22:55:15 -0800

    102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def complex_delete(a_dictionary, value):
     tmp = a_dictionary.copy()
-    for k, v in temp.items():
+    for k, v in tmp.items():
         if value == v:
             a_dictionary.pop(k)
     return a_dictionary
