ff313ede24a204e9236b5bc1f636235692e9d504
lynnchepp <lynn286035@gmail.com>
2023-12-18 22:25:53 -0800

    0-square.py

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,6 @@
+#!/usr/bin/pythom3
+"""Amodule for square"""
+
+class Square:
+    """Represent a square."""
+    pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/pythom3
+"""Square module."""
+
+class Square:
+    """Define a square."""
+    def _init_(self, size):
+        """Constructor.
+        Args:
+        size: length of side of the square.
+        """
+
+        self. _size = size
